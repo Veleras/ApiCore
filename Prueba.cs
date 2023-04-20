64d99483b2bfd21a44d68399331c3c80c130128e
David Aguilar <david.aguilar@atmira.com>
2023-04-20 19:55:20 +0200

    PruebaFichero

---
+++ Prueba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueba
+{
+    public class Prueba
+    {
+        public string PropiedadUno { get; set; }
+        public string PropiedadDos { get; set; }
+    }
+}
